84a2f495cb69a2d898fa7a90ab62f43f60bb1c43
DilshadHussain <DilshadHussain@users.noreply.github.com>
2024-11-16 14:38:24 +0530

    Update app_jira_read_not_working.py

--- app_jira_read_not_working.py
+++ app_jira_read_not_working.py
@@ -1,15 +1,1 @@
-from atlassian import Jira
 
-# Replace with your Jira instance URL and credentials
-jira = Jira(
-    url='http://ber-jirapp-001v.hub.calyxhosting.com:8080/',
-    username='hussaid',
-    password='Year2024.09'
-)
-
-# JQL query to fetch issues
-jql = 'project = CMT AND status IN ("In UAT") ORDER BY issuekey'
-issues = jira.jql(jql)
-
-# Print the retrieved issues
-print(issues)
